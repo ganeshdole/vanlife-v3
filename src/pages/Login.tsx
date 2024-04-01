64540c0d45b46688f37cd2192335b55efe66ae0d
Ganesh Dole <94167146+ganeshdole@users.noreply.github.com>
2024-04-02 02:27:33 +0530

    .

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -11,32 +11,34 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button"
+import { Link } from 'react-router-dom';
 
 const LogInPage: React.FC = () => {
    return(
     <div className='flex min-h-screen bg-orange-50'>
         <div className='hidden md:inline-block md:flex-col w-full object-cover'>
-            <img src="./src/assets/images/login.jpg" alt="Login page image" className='object-cover w-full h-full scale-x-[-1]' />
+            <img src="./src/assets/images/login.jpg" alt="Login page image" className='object-cover w-full h-full scale-x-[-1] object-right' />
         </div>
         <div className='flex w-full justify-center items-center'>
-            <Card className='w-[400px] '>
+            <Card className='w-[400px] border-none bg-inherit'>
                 <div className='flex flex-col justify-center'>
                     <CardHeader>
-                        <CardTitle>Sign in</CardTitle>
+                        <CardTitle>Log in</CardTitle>
                         <CardDescription>Log in to your account to continue</CardDescription>
                     </CardHeader>    
                     <CardContent>
                         <form className='flex flex-col gap-4'>
                         <Label htmlFor="email">Email</Label>
-                        <Input type="email" placeholder="jhon@vanlife.com" id="email" />
+                        <Input type="email" placeholder="jhon@vanlife.com" id="email"  />
                         <Label htmlFor="password">Password</Label>
                         <Input type="password" placeholder="password" id="password" />
-                            <Button>Sign in</Button>
+                            <Button>Log in</Button>
                         </form>
+                        <Link  to="/" className='flex justify-center mt-1'>Forgot password?</Link>
                     </CardContent>
-                    <CardFooter className='flex  flex-row justify-between'>
-                        <Button>Forgot password?</Button>
-                        <Button>Create an account</Button>
+                    <CardFooter className='flex flex-row justify-between'>
+                        <p>Don't have an account?</p>
+                        <Button>Sign up</Button>
                     </CardFooter>
                 </div>
             </Card>
