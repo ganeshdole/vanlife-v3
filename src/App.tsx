678bc7b22bd67b5d73530773c91691da466e98a0
Ganesh Dole <94167146+ganeshdole@users.noreply.github.com>
2024-04-02 09:09:02 +0530

    Started with van pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 import Layout from "./components/Layouts/Layout";
 import About from "./pages/About";
 import LogInPage from "./pages/Login";
-
+import Vans from "./pages/Vans/Vans";
 
 export default function App() {
   return (
@@ -11,6 +11,7 @@ export default function App() {
       <Route element={<Layout/>}>
         <Route index element={<Home/>}/>
         <Route path="about" element={<About/>}/>
+        <Route path="vans" element={<Vans/>}/>
       </Route>
       <Route path="/login" element={<LogInPage/>}/>
     </Routes>
---
+++ src/pages/Vans/Vans.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Vans: React.FC = () => {
+    return (
+        <div>
+            <h1>Vans Page</h1>
+            {/* Add your Vans component code here */}
+        </div>
+    );
+};
+
+export default Vans;
