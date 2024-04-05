ea9baea04e6c876c91a70fd67891372545b2f44f
Ganesh Dole <94167146+ganeshdole@users.noreply.github.com>
2024-04-05 11:09:47 +0530

    .

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,26 +1,25 @@
-
 import { NavLink } from "react-router-dom";
-import {Button} from "../ui/button";
+import { Button } from "../ui/button";
 
-
-
-function Navbar()
-{
-    return(
-      <>
-      <NavLink to="/" 
-      className="text-2xl font-extrabold" >
-            #VANLIFE
+function Navbar() {
+  return (
+    <>
+      <NavLink to="/" className="text-2xl font-extrabold">
+        #VANLIFE
       </NavLink>
-      <nav className="flex ml-auto gap-10 "
-      >
-        <Menu/>
+      <nav className="flex ml-auto gap-10 ">
+        <Menu />
       </nav>
       <div>
-       <Button> <NavLink to="/login" className="nav-NavLink px-2"  >Login</NavLink></Button>
+        <Button>
+          {" "}
+          <NavLink to="/login" className="nav-NavLink px-2">
+            Login
+          </NavLink>
+        </Button>
       </div>
-      </>
-    )
+    </>
+  );
 }
 
 export default Navbar;
@@ -30,24 +29,23 @@ function Menu() {
     <div className="nav-NavLinks  px-5 hidden sm:block">
       <ul className="flex gap-10">
         <li>
-          <NavLink to="/host" className="font-semibold nav-NavLink" >Host</NavLink>
+          <NavLink to="/host" className="font-semibold nav-NavLink">
+            Host
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/about" className="font-semibold  nav-NavLink">About</NavLink>
+          <NavLink to="/about" className="font-semibold  nav-NavLink">
+            About
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/vans" className="font-semibold  nav-NavLink">Our Vans</NavLink>
+          <NavLink to="/vans" className="font-semibold  nav-NavLink">
+            Our Vans
+          </NavLink>
         </li>
       </ul>
     </div>
   );
 }
 
-
 /*
-className={`font-semibold  nav-NavLink ${({ isActive }: { isActive: boolean }) =>
-        [
-          isActive ? "hover:underline hover:font-bold" : "",
-        ].join(" ")
-      }`}
-*/
